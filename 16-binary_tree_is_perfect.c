97c2f0c1508fd5a1e564793a17c8d96879a004ed
CodingWithDana <danadaole@gmail.com>
2025-08-08 05:48:20 +0000

    check if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,13 +22,14 @@ static int find_depth(const binary_tree_t *node)
 }
 
 /**
- * recursively_check_perfect_subtree - helper function to recursively check if every subtree is perfect
+ * recursively_check_perfect_subtree - check if every subtree is perfect
  * @tree: pointer pointing the subtree to be checked
  * @depth: expected depth of all leaves (from function find_depth)
  * @level: current level of node in the tree (root level = 0)
  * Return: 1 if subtree is perfect, if not then 0
  */
-static int recursively_check_perfect_subtree(const binary_tree_t *tree, int depth, int level)
+static int recursively_check_perfect_subtree(const binary_tree_t *tree,
+		int depth, int level)
 {
 	/* If the current node is NULL, this subtree is not perfect */
 	if (tree == NULL)
@@ -42,11 +43,15 @@ static int recursively_check_perfect_subtree(const binary_tree_t *tree, int dept
 		/* Here, depth is the expected leaf depth, and */
 		/* +1 for the actual depth of this leaf */
 		if (depth == level + 1)
+		{
 			/* Leaf at correct depth, perfect, return 1 */
 			return (1);
+		}
 		else
+		{
 			/* Leaf at incorrect depth, not perfect, return 0 */
 			return (0);
+		}
 	}
 	/* If a node has only one child, tree is not full, so not perfect */
 	if (tree->left == NULL || tree->right == NULL)
@@ -55,9 +60,13 @@ static int recursively_check_perfect_subtree(const binary_tree_t *tree, int dept
 	/* incrementing level as we go deeper */
 	if (recursively_check_perfect_subtree(tree->left, depth, level + 1) &&
 		recursively_check_perfect_subtree(tree->right, depth, level + 1))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
 
 /**
